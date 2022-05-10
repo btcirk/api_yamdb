4683c6e2529a6f98a9760cbdc39b84c6bb87f1b0
vvb <vvb@playtronix.net>
2022-05-10 14:33:38 +0800

    Fix user roles

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    ADMIN = 'a'
+    MODERATOR = 'm'
+    USER = 'u'
+
+    ROLE_CHOICES = (
+        (ADMIN, 'admin'),
+        (MODERATOR, 'moderator'),
+        (USER, 'user'),
+    )
+    role = models.CharField('Роль пользователя', max_length=1, choices=ROLE_CHOICES, default=USER)
+    bio = models.TextField('Биография', blank=True,)
